f16ad36c2916bda90333489f631304bef99b8f06
Jorge Dominguez <cabronator_pesuti@hotmail.com>
2026-02-02 12:59:11 -0300

    fix build

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,10 +1,14 @@
 import pino from 'pino';
 
-export const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-    },
-  },
-});
+const isProd = process.env.NODE_ENV === 'production';
+
+export const logger = isProd
+  ? pino()
+  : pino({
+      transport: {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+        },
+      },
+    });
